14dfdcfd7e22d251826f41583d060d59dd9d1e7b
yuvalns7 <62835509+AlmogKaha@users.noreply.github.com>
2023-03-14 22:47:03 +0200

    next

--- backend/middlewares/authMiddleware.js
+++ backend/middlewares/authMiddleware.js
@@ -12,7 +12,7 @@ const protect = asyncHandler(async (req, res, next) => {
   //     token = req.headers.authorization.split(" ")[1]
   //     const decoded = jwt.verify(token, process.env.JWT_SECRET)
   //     req.user = await User.findById(decoded.id).select("-password")
-  //     next()
+  next()
   //   } catch (error) {
   //     console.error(error)
   //     res.status(401)
@@ -27,7 +27,7 @@ const protect = asyncHandler(async (req, res, next) => {
 
 const admin = (req, res, next) => {
   // if (req.user && req.user.isAdmin) {
-  //   next()
+  next()
   // } else {
   //   res.status(401)
   //   throw new Error("Not authorized as an admin")
