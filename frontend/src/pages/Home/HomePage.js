331ea3adedf01b5429fb96b8ce512b1b8727a4f6
yuvalns7 <62835509+AlmogKaha@users.noreply.github.com>
2023-03-14 22:14:42 +0200

    remove

--- frontend/src/pages/Home/HomePage.js
+++ frontend/src/pages/Home/HomePage.js
@@ -1,39 +1,38 @@
-import React, { useEffect } from "react";
-import { useParams } from "react-router-dom";
-import { useDispatch, useSelector } from "react-redux";
-import Book from "../../components/Book/Book";
-import Message from "../../components/Message/Message";
-import Loader from "../../components/Loader/Loader";
-import Paginate from "../../components/Paginate/Paginate";
-import { listBooks } from "../../actions/bookActions";
-import "./HomePage.css";
+import React, { useEffect } from "react"
+import { useParams } from "react-router-dom"
+import { useDispatch, useSelector } from "react-redux"
+import Book from "../../components/Book/Book"
+import Message from "../../components/Message/Message"
+import Loader from "../../components/Loader/Loader"
+import Paginate from "../../components/Paginate/Paginate"
+import { listBooks } from "../../actions/bookActions"
+import "./HomePage.css"
 
 const HomePage = () => {
-  const { keyword } = useParams();
-  const { pageNumber } = useParams() || 1;
+  const { keyword } = useParams()
+  const { pageNumber } = useParams() || 1
 
-  const dispatch = useDispatch();
+  const dispatch = useDispatch()
 
-  const bookList = useSelector((state) => state.bookList);
-  const { loading, error, books, page, pages } = bookList;
+  const bookList = useSelector((state) => state.bookList)
+  const { loading, error, books, page, pages } = bookList
 
   useEffect(() => {
-    dispatch(listBooks(keyword, pageNumber));
-  }, [dispatch, keyword, pageNumber]);
+    dispatch(listBooks(keyword, pageNumber))
+  }, [dispatch, keyword, pageNumber])
 
   return (
-    <div className="home">
-      <div className="title">
+    <div className='home'>
+      <div className='title'>
         <h2>All Books</h2>
-        <p>Mua sách truyện tiếng Anh, sách ngoại văn.</p>
       </div>
       {loading ? (
         <Loader />
       ) : error ? (
-        <Message variant="alert-danger">{error}</Message>
+        <Message variant='alert-danger'>{error}</Message>
       ) : (
         <>
-          <div className="books">
+          <div className='books'>
             {books.map((book) => (
               <Book key={book._id} book={book} />
             ))}
@@ -46,7 +45,7 @@ const HomePage = () => {
         </>
       )}
     </div>
-  );
-};
+  )
+}
 
-export default HomePage;
+export default HomePage
